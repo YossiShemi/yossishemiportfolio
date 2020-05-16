6bb91ebf31d0717056bd9c761f3fa2343afefdb1
Yossi Shemi <yossi20941@gmail.com>
2020-05-16 23:10:28 +0300

    Updates

---
+++ precache-manifest.c0fcdbec96fca612baa9484d2576a5d8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aefc3b34e10b66425107f51804bbdbc6",
+    "url": "/yossishemiportfolio/index.html"
+  },
+  {
+    "revision": "c9f49197cb23c22bea07",
+    "url": "/yossishemiportfolio/static/css/main.6f2d849d.chunk.css"
+  },
+  {
+    "revision": "54f6c8c83231bce791e4",
+    "url": "/yossishemiportfolio/static/js/2.78f68fe0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/yossishemiportfolio/static/js/2.78f68fe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9f49197cb23c22bea07",
+    "url": "/yossishemiportfolio/static/js/main.fc7ca400.chunk.js"
+  },
+  {
+    "revision": "2ac17f67c5f8648da7b9",
+    "url": "/yossishemiportfolio/static/js/runtime-main.a19d1181.js"
+  },
+  {
+    "revision": "505a237c20e794dca339f23ae14970a1",
+    "url": "/yossishemiportfolio/static/media/backgammonWeb.505a237c.png"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/yossishemiportfolio/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "3aac835f497aa7e9208a2b25670ea73e",
+    "url": "/yossishemiportfolio/static/media/logo.3aac835f.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/yossishemiportfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "16891745377af3653f8e5896f40900c4",
+    "url": "/yossishemiportfolio/static/media/working.16891745.jpg"
+  }
+]);
